a324fa97d2e10bb4775fd67b59ccc81cda68ee1e
Petr Ivanov <petr.yrs@gmail.com>
2023-06-23 10:54:48 +0300

    Расширены Items

--- src/Request/Receipt/ReceiptItem.php
+++ src/Request/Receipt/ReceiptItem.php
@@ -13,26 +13,33 @@ class ReceiptItem extends BaseRequest
     public string                      $price;
     public string                      $quantity;
     public string                      $amount;
-    public ?string                     $vat                      = null;
-    public ?string                     $method                   = null;
-    public ?string                     $object                   = null;
-    public ?string                     $measurementUnit          = null;
-    public ?string                     $excise                   = null;
-    public ?string                     $countryOriginCode        = null;
-    public ?string                     $customsDeclarationNumber = null;
-    public ?string                     $agentSign                = null;
-    public ?ReceiptItemAgentData       $agentData                = null;
-    public ?ReceiptItemPurveyorData    $purveyorData             = null;
-    public ?ReceiptItemProductCodeData $productCodeData          = null;
-    public ?string                     $additionalPositionInfo   = null;
+    public string                      $vat;
+    public ?string                     $method                      = null;
+    public ?string                     $object                      = null;
+    public ?string                     $measurementUnit             = null;
+    public ?string                     $excise                      = null;
+    public ?string                     $countryOriginCode           = null;
+    public ?string                     $customsDeclarationNumber    = null;
+    public ?string                     $agentSign                   = null;
+    public ?ReceiptItemAgentData       $agentData                   = null;
+    public ?ReceiptItemPurveyorData    $purveyorData                = null;
+    public ?string                     $additionalPositionInfo      = null;
+    public ?string                     $saleObjectData              = null;
+    public ?array                      $industryRequisiteCollection = null;
+    public ?ReceiptItemProductCodeData $productCodeData             = null;
+    public ?array                      $markPartQuantity            = null;
+    public ?string                     $rawMarkCode                 = null;
+    public ?string                     $groupSeparator              = null;
+    public ?string                     $unitCode;
+
 
     /**
      * @param string $label
      * @param string $price
      * @param string $amount
-     * @param string|null $vat
+     * @param string $vat
      */
-    public function __construct(string $label, string $price, string $amount, ?string $vat = null)
+    public function __construct(string $label, string $price, string $amount, string $vat)
     {
         $this->label  = $label;
         $this->price  = $price;
